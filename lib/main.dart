b9d30a4180c064283dc741f926bbf3fe97c6fb74
ahmad-cod <ahmadadewale800@gmail.com>
2024-07-02 12:56:17 +0100

    Ensure a cohesive design

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: Typography.whiteHelsinki
+        primaryColor: Colors.grey[850],
+        textTheme: Typography.whiteHelsinki,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey[800],
+          iconTheme: IconThemeData(
+            color: Colors.grey[300]
+          ),
+          titleTextStyle: const TextStyle(
+            color: Colors.white70,
+            fontSize: 16,
+          )
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color.fromARGB(174, 9, 128, 225),
+          foregroundColor: Colors.white70,
+        ),
+        scaffoldBackgroundColor: Colors.grey[850],
       ),
       home: const HomePage(),
     );
